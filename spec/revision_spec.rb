fdac5e8ed419bf11cb4320564531b6800317e00b
Rafael de F. Ferreira <public@rafaelferreira.net>
2014-03-29 19:53:58 -0300

    Refactoring init repository in tests

--- spec/revision_spec.rb
+++ spec/revision_spec.rb
@@ -3,21 +3,20 @@ require 'fileutils'
 
 describe Historia::Revision do
   @repo, @first_sha, @first_short, @second_sha, @second_short = [nil, nil, nil, nil, nil]
-  let (:repo_dir)  { 'spec/support/fixtures/a' }
   attr_reader :repo
   attr_reader :first_sha
   attr_reader :first_sha
   attr_reader :second_sha
   attr_reader :second_short
 
-  before (:all) {
+  def init(name)
+    repo_dir = "spec/support/fixtures/#{name}"
     FileUtils.rm_rf repo_dir
     FileUtils.mkdir_p repo_dir
-    @repo = Rugged::Repository.init_at repo_dir
+    Rugged::Repository.init_at repo_dir
+  end
 
-    oid = repo.write("x", :blob)
-    index = Rugged::Index.new.tap{|index| index.add path: "f", oid: oid, mode:  0100644}
-
+  def commits n
     commit1 = Rugged::Commit.create(repo,  {message: "1",
                                   update_ref: 'HEAD',
                                   tree: index.write_tree(repo),
@@ -34,6 +33,13 @@ describe Historia::Revision do
                                   parents: [commit1]})
     @second_sha = commit2
     @second_short = @second_sha[0..6]
+
+  end
+ 
+  before (:all) {
+    @repo = init 'a'
+    oid = repo.write("x", :blob)
+    index = Rugged::Index.new.tap{|index| index.add path: "f", oid: oid, mode:  0100644}
 
     repo.tags.create('v1', repo.head.target, {message: 'msg'})
   }
