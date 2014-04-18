eb67ee6748372bd1c8537b5f173cf5144c9f67a5
Rafael de F. Ferreira <public@rafaelferreira.net>
2014-04-18 16:52:24 -0300

    All tests now creating their own fixtures

--- spec/range_spec.rb
+++ spec/range_spec.rb
@@ -2,58 +2,61 @@ require 'spec_helper'
 
 describe Historia::Range do
   let (:walker) { mock }
-  let (:repo) { Rugged::Repository.new 'spec/support/fixtures/house' }
-  let (:commit) { repo.head.target }
+
+  attr_reader :repo
+  attr_reader :head_left
+  attr_reader :head_right
+  attr_reader :common
   
   before (:all) {
-    @repo = init "a" 
+    @repo = init "house" 
 
-    first = commit @repo
+    common_0 = commit @repo
+    @common = commit @repo, common_0
 
-    @second = commit @repo, first
-
-    repo.tags.create('v1', repo.head.target, {message: 'msg'})
-  }
+    @head_left  = commit @repo, @common
+    @head_right = commit @repo, @common
+  } 
 
   context "parse" do
     it "a single sha becomes a single push" do
-      range = Historia::Range.new repo, "af95734"
+      range = Historia::Range.new repo, (short head_left)
 
-      walker.should_receive(:push).with "af95734f23943f2829692b45be7fa7abe3e906db" 
+      walker.should_receive(:push).with head_left
 
       range.apply_to walker
     end
 
     it "a single negated sha becomes a single hide" do
-      range = Historia::Range.new repo, "^5b2691f"
+      range = Historia::Range.new repo, "^#{short head_right}"
 
-      walker.should_receive(:hide).with "5b2691f3cf7a544dcff3463ff0ce34204aa2dba0" 
+      walker.should_receive(:hide).with head_right 
 
       range.apply_to walker
     end
 
     it "several shas become several pushes" do
-      range = Historia::Range.new repo, "af95734 5b2691f"
+      range = Historia::Range.new repo, "#{short head_left} #{short head_right}"
 
-      walker.should_receive(:push).with "af95734f23943f2829692b45be7fa7abe3e906db"
-      walker.should_receive(:push).with "5b2691f3cf7a544dcff3463ff0ce34204aa2dba0"
+      walker.should_receive(:push).with head_left
+      walker.should_receive(:push).with head_right
       range.apply_to walker
     end
 
     it "parses a mix of pushes and hides" do
-      range = Historia::Range.new repo, "af95734 ^5b2691f"
+      range = Historia::Range.new repo, "#{short head_left} ^#{short head_right}"
 
-      walker.should_receive(:push).with "af95734f23943f2829692b45be7fa7abe3e906db"
-      walker.should_receive(:hide).with "5b2691f3cf7a544dcff3463ff0ce34204aa2dba0"
+      walker.should_receive(:push).with head_left
+      walker.should_receive(:hide).with head_right
 
       range.apply_to walker
     end
 
     it "understands a range given by two dots" do
-      range = Historia::Range.new repo, "5721551..5b2691f"
+      range = Historia::Range.new repo, "#{short common}..#{short head_right}"
 
-      walker.should_receive(:hide).with "57215519751903cbe2be1a3ec9599093f2def119"
-      walker.should_receive(:push).with "5b2691f3cf7a544dcff3463ff0ce34204aa2dba0"
+      walker.should_receive(:hide).with common
+      walker.should_receive(:push).with head_right
 
       range.apply_to walker
     end
